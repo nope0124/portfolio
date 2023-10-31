7ae3e97479502a7bf9b9f8ffc9e0d8187805612c
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-11-01 01:24:31 +0900

    インターン追加

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -31,13 +31,15 @@ const Timeline = () => {
             style={{ left: "15px" }}
           ></div>
           <ul className="list-none m-0 p-0">
+            <Item date="2023/09 ~ 2023/10" title="RECRUIT Internship for Engineers, Data Specialists 2023"/>
+            <Item date="2023/08" title="GREE Camp 2023" />
             <Item date="2023/07" title="株式会社Cygames サマーインターン" />
             <Item date="2023/04" title="大学院入学" />
             <Item date="2023/03" title="PIXIV SPRING BOOT CAMP 2023" />
             <Item date="2022/11 ~ " title="株式会社DONUTS 長期インターン" />
             <Item date="2022/03" title="ICPCアジア地区横浜大会2021出場 (Team:shichifuku)" />
             <Item date="2021/11" title="株式会社Cygames 短期インターン" />
-            <Item date="2021/07 ~ " title="株式会社インフラトップ 長期インターン" />
+            <Item date="2021/07 ~ 2022/12" title="株式会社インフラトップ 長期インターン" />
             <Item date="2021/03" title="ICPCアジア地区横浜大会2020出場 (Team:shichifuku)" />
             <Item date="2020/05" title="AtCoder水達成" />
             <Item date="2019/11 ~ 2020/12" title="母校パソコン部部活動指導員" />
